dce2ddbf813996cceea04f9bad21f5f3bc2b0d0e
pehu71 <humplikpetr@gmail.com>
2017-01-20 20:50:07 +0100

    mixing with TS - part2

--- script/ts-module/ts-module.js
+++ script/ts-module/ts-module.js
@@ -1,16 +1,16 @@
 angular.module('tsModule', []); // this is the little hack to integrate with full-flavoured TS module
 var tsModule;
 (function (tsModule) {
-    var tsm = angular.module('tsModule');
+    var tsm = angular.module('tsModule'); // reference the fake angular module to add controller and directive to it
     var SampleController = (function () {
         function SampleController($location) {
-            this.path = '';
-            this.path = 'I know my $location.path(): ' + $location.path();
+            this.path = ''; // use types
+            this.path = "I know my $location.path(): " + $location.path(); // you can use template strings
         }
-        SampleController.$inject = ['$location'];
+        SampleController.$inject = ['$location']; // let it survive uglification
         return SampleController;
     }());
     tsModule.SampleController = SampleController;
-    tsm.controller('SampleController', tsModule.SampleController);
+    tsm.controller('SampleController', tsModule.SampleController); // add controller to angular module
 })(tsModule || (tsModule = {}));
 //# sourceMappingURL=ts-module.js.map
--- script/ts-module/ts-module.ts
+++ script/ts-module/ts-module.ts
@@ -2,20 +2,20 @@ angular.module('tsModule', []); // this is the little hack to integrate with ful
 
 module tsModule {
 
-    import ILocationService = angular.ILocationService;
+    let tsm = angular.module('tsModule'); // reference the fake angular module to add controller and directive to it
 
-    let tsm = angular.module('tsModule');
+    import ILocationService = angular.ILocationService; // import some types
 
     export class SampleController {
 
-        path: string = '';
+        path: string = ''; // use types
 
-        constructor($location: ILocationService) {
-            this.path = 'I know my $location.path(): ' + $location.path();
+        constructor($location: ILocationService) {  // use strongly typed DI
+            this.path = `I know my $location.path(): ${$location.path()}`; // you can use template strings
         }
 
-        static $inject = ['$location'];
+        static $inject = ['$location']; // let it survive uglification
     }
 
-    tsm.controller('SampleController', tsModule.SampleController);
+    tsm.controller('SampleController', tsModule.SampleController); // add controller to angular module
 }
