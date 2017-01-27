ee07e5f2d2b1d67dc5ce861fd4338707f91782fd
pehu71 <humplikpetr@gmail.com>
2017-01-27 14:54:23 +0100

    component

--- script/components/sample-components.js
+++ script/components/sample-components.js
@@ -1,23 +1,23 @@
 angular.module('sample-components', [])
 
-    .controller('listComponentCtrl', function ($scope, $element, $attrs) {
-        var ctrl = this;
-        ctrl.sins = [{title:'gluttony', severity:0}, {title:'adultery',severity:1} ];
-        ctrl.quote = 'sdfd ade ew wewee qwewqe wewe weew';
-
-        ctrl.clickAlert = function () {
-            ctrl.sins.push({title:'clicking', severity:10})
-        }
-    })
     .component('listComponent', {
 
         bindings: {
-            showQuote: '<'
+            show: '<'
         },
 
-        controller: 'listComponentCtrl',
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            ctrl.sins = [{title: 'gluttony', severity: 0}, {title: 'adultery', severity: 1}];
+            ctrl.quote = 'sdfd ade ew wewee qwewqe wewe weew';
+
+            ctrl.addSin = function () {
+                ctrl.sins.push({title: 'clicking', severity: 10})
+            };
+
+        },
         controllerAs: 'listCtrl',
-        template: '<div><p>{{listCtrl.quote}}</p><p ng-repeat="sin in listCtrl.sins" ng-click="listCtrl.clickAlert()">{{sin.title}}</p></div>'
+        template: '<div><p ng-if="listCtrl.show">{{listCtrl.quote}}</p><p ng-repeat="sin in listCtrl.sins" ng-click="listCtrl.addSin()">{{sin.title}}</p></div>'
 
     });
 
