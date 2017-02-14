562a6036597ec48b3e7731400b0f8d066030c542
pehu71 <humplikpetr@gmail.com>
2017-02-14 19:17:44 +0100

    some comments

--- chapters/c11/controllers-tests.js
+++ chapters/c11/controllers-tests.js
@@ -11,8 +11,16 @@ describe('controllers', function () {
             $scope;
 
         beforeEach(inject(function(_$controller_){
+
+            // we get hold of $controller service via DI
+            // the underscores are here so as we could name it $controller - injector removes them during matching
             $controller = _$controller_;
+
+            // we define some scope
             $scope = {};
+
+            // we create controller instance through the service
+            // and pass the created $scope to the constructor as locals
             controller = $controller('customerController', {$scope: $scope});
         }));
 
@@ -29,8 +37,6 @@ describe('controllers', function () {
                 var result = $scope.checkCustomerSn();
                 expect(result).toBe(false);
             });
-
-
         });
 
 
